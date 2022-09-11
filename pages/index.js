c8b161402e07459f28478fa5b23456f3a5dd7bf4
Alex-new-laptop <acontell@gmail.com>
2022-09-11 10:05:45 -0400

    design update

--- pages/index.js
+++ pages/index.js
@@ -12,3 +12,32 @@ export default function Home() {
     </div>
   );
 }
+
+export async function getStaticProps() {
+  const results = await fetch(
+    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/folders/`,
+    {
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          process.env.CLOUDINARY_API_KEY +
+            ':' +
+            process.env.CLOUDINARY_API_SECRET
+        ).toString('base64')}`,
+      },
+    }
+  ).then((r) => r.json());
+
+  // const { resources } = results;
+  // const images = resources.map((resource) => {
+  //   return {
+  //     id: resource.asset_id,
+  //     title: resource.public_id;
+
+  //   };
+  // });
+
+  console.log(results);
+  return {
+    props: {},
+  };
+}
