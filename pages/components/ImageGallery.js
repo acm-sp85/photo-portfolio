724cfa4e17403c3243879ed41d91a23e46dc5431
Alex-new-laptop <acontell@gmail.com>
2022-11-26 19:41:59 -0500

    material UI Masonry

--- pages/components/ImageGallery.js
+++ pages/components/ImageGallery.js
@@ -3,10 +3,12 @@ import { useState, useEffect } from 'react';
 import styles from './../../styles/Home.module.scss';
 
 import Image from 'next/image';
-import { XMasonry, XBlock } from 'react-xmasonry';
-// import { Masonry } from '@mui/lab';
+
 import ViewModuleIcon from '@mui/icons-material/ViewModule';
 import ZoomOutMapIcon from '@mui/icons-material/ZoomOutMap';
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
 export default function ImageGallery(props) {
   let [photos, setPhotos] = useState(props.images);
@@ -35,33 +37,25 @@ export default function ImageGallery(props) {
       )}
 
       {showGrid && (
-        <XMasonry
-          maxColumns={3}
-          responsive={true}
-          smartUpdate={true}
-          className={styles.photo__display}
-        >
-          {photos.map((image) => {
-            startingPoint++;
-            return (
-              <XBlock key={startingPoint}>
-                <div className={styles.card}>
-                  <Image
-                    width={image.width}
-                    height={image.height}
-                    src={image.image}
-                    alt={startingPoint} //adding a counter to be know which image we clicked
-                    onClick={(e) => {
-                      console.log(e.target.alt);
-                      setStartingPoint(e.target.alt);
-                      gridSwitch();
-                    }}
-                  />
-                </div>
-              </XBlock>
-            );
-          })}
-        </XMasonry>
+        <Box sx={{ overflowY: 'scroll' }}>
+          <ImageList variant="masonry" cols={3} gap={8}>
+            {photos.map((image) => (
+              <ImageListItem key={image.img}>
+                <Image
+                  width={image.width}
+                  height={image.height}
+                  src={image.image}
+                  alt={startingPoint} //adding a counter to be know which image we clicked
+                  onClick={(e) => {
+                    console.log(e.target.alt);
+                    setStartingPoint(e.target.alt);
+                    gridSwitch();
+                  }}
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </Box>
       )}
     </div>
   );
