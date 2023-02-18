f3bac9c7702d9b173a416933c35807809cfa8fde
Alex-new-laptop <acontell@gmail.com>
2023-02-18 10:11:06 -0500

    fixing paths for commissioned

--- pages/commissioned/[commissionedName].js
+++ pages/commissioned/[commissionedName].js
@@ -7,7 +7,7 @@ import ReactPlayer from 'react-player';
 
 export default function Commissioned({ images, nextCursor, folders }) {
   let router = useRouter();
-  let folder = router.query.assignmentName;
+  let folder = router.query.commissioned;
 
   return (
     <div className={styles.assignment__block}>
@@ -18,9 +18,9 @@ export default function Commissioned({ images, nextCursor, folders }) {
 
 // via getStaticProps we are making our API call
 export async function getStaticProps(context) {
-  const assignmentName = context.params.assignmentName;
+  const commissioned = context.params.commissioned;
   const results = await search({
-    expression: `folder="photo-portfolio/commercial/${assignmentName}"`,
+    expression: `folder="photo-portfolio/commercial/${commissioned}"`,
   });
 
   const { resources, next_cursor: nextCursor } = results;
@@ -37,40 +37,40 @@ export async function getStaticProps(context) {
 export async function getStaticPaths() {
   return {
     paths: [
-      { params: { assignmentName: 'chase-bank' } },
-      { params: { assignmentName: 'vogue' } },
-      { params: { assignmentName: 'telva' } },
-      { params: { assignmentName: 'off-white' } },
-      { params: { assignmentName: 'sylvain' } },
-      { params: { assignmentName: 'pace' } },
-      { params: { assignmentName: 'demelier' } },
-      { params: { assignmentName: 'ismine' } },
-      { params: { assignmentName: 'kionita' } },
-      { params: { assignmentName: 'wok-bao' } },
-      { params: { assignmentName: 'dazzler' } },
-      { params: { assignmentName: 'stone-island' } },
-      { params: { assignmentName: 'balmain' } },
-      { params: { assignmentName: 'carolina-herrera' } },
-      { params: { assignmentName: 'luxottica' } },
-      { params: { assignmentName: 'uag' } },
-      { params: { assignmentName: 'gift-of-time' } },
-      { params: { assignmentName: 'church-st' } },
-      { params: { assignmentName: 'blink' } },
-      { params: { assignmentName: 'mrka' } },
-      { params: { assignmentName: 'shipibo' } },
-      { params: { assignmentName: 'hergueta' } },
-      { params: { assignmentName: 'lonely-clay' } },
-      { params: { assignmentName: 'coma-store' } },
-      { params: { assignmentName: 'henry-julier' } },
-      { params: { assignmentName: 'arizona' } },
-      { params: { assignmentName: 'olea' } },
-      { params: { assignmentName: 'perrier' } },
-      { params: { assignmentName: 'sunday-times' } },
-      { params: { assignmentName: 'usb' } },
-      { params: { assignmentName: 'beto' } },
-      { params: { assignmentName: 'airama' } },
-      { params: { assignmentName: 'nuvolari' } },
-      { params: { assignmentName: 'bullish' } },
+      { params: { commissioned: 'chase-bank' } },
+      { params: { commissioned: 'vogue' } },
+      { params: { commissioned: 'telva' } },
+      { params: { commissioned: 'off-white' } },
+      { params: { commissioned: 'sylvain' } },
+      { params: { commissioned: 'pace' } },
+      { params: { commissioned: 'demelier' } },
+      { params: { commissioned: 'ismine' } },
+      { params: { commissioned: 'kionita' } },
+      { params: { commissioned: 'wok-bao' } },
+      { params: { commissioned: 'dazzler' } },
+      { params: { commissioned: 'stone-island' } },
+      { params: { commissioned: 'balmain' } },
+      { params: { commissioned: 'carolina-herrera' } },
+      { params: { commissioned: 'luxottica' } },
+      { params: { commissioned: 'uag' } },
+      { params: { commissioned: 'gift-of-time' } },
+      { params: { commissioned: 'church-st' } },
+      { params: { commissioned: 'blink' } },
+      { params: { commissioned: 'mrka' } },
+      { params: { commissioned: 'shipibo' } },
+      { params: { commissioned: 'hergueta' } },
+      { params: { commissioned: 'lonely-clay' } },
+      { params: { commissioned: 'coma-store' } },
+      { params: { commissioned: 'henry-julier' } },
+      { params: { commissioned: 'arizona' } },
+      { params: { commissioned: 'olea' } },
+      { params: { commissioned: 'perrier' } },
+      { params: { commissioned: 'sunday-times' } },
+      { params: { commissioned: 'usb' } },
+      { params: { commissioned: 'beto' } },
+      { params: { commissioned: 'airama' } },
+      { params: { commissioned: 'nuvolari' } },
+      { params: { commissioned: 'bullish' } },
     ],
     fallback: false,
   };
